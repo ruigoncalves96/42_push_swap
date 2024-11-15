9a9dbbc8daceafff4a0166510e3e9643fa37c81f
Rui Gonçalves <ruigoncalves@unknownbe0bda3dfc69.lan>
2024-11-15 17:26:36 +0000

    15/11 17:26

--- src/algorithm.c
+++ src/algorithm.c
@@ -6,27 +6,37 @@
 /*   By: randrade <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/12 16:28:22 by randrade          #+#    #+#             */
-/*   Updated: 2024/11/14 18:21:34 by randrade         ###   ########.fr       */
+/*   Updated: 2024/11/15 17:20:43 by randrade         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 # include "push_swap.h"
 
-//unsigned int	ft_count_is_sorted(t_list *stack_a)
-//{
-//	t_list	*i;
-//	unsigned int	count;
-//
-//	count = 0;
-//	i = stack_a;
-//	while (i->next && i->nbr < i->next->nbr)
-//	{
-//		i = i->next;
-//		count++;
-//	}
-//	return (count);
-//}
-//
+
+void	ft_algorithm(t_list **stack_a, t_list **stack_b)
+{
+	t_list	*last;
+
+	while (ft_is_sorted(*stack_a) == false)
+	{
+		last = ft_lstlast(*stack_a);
+		if ((*stack_a)->nbr > (*stack_a)->next->nbr)
+			ft_swap(stack_a, stack_b, SA);
+		else if ((*stack_a)->nbr > (*stack_a)->next->nbr && (*stack_a)->nbr > last->nbr )
+			ft_rotate(stack_a, stack_b, RA);
+		else if ((*stack_a)->nbr < last->nbr && (*stack_a)->nbr < (*stack_a)->next->nbr && last->nbr < (*stack_a)->next->nbr)
+		{
+			ft_reverse_rotate(stack_a, stack_b, RRA);
+			ft_swap(stack_a, stack_b, SA);
+		}
+		else 
+			ft_rotate(stack_a, stack_b, RA);
+	}
+}
+
+
+
+/*		ORGANIZE BY MINIMUM NUMBER
 void	ft_get_info(t_list_info *list_info, t_list **stack_a)
 {
 	t_list	*check;
@@ -69,3 +79,21 @@ void	ft_algorithm(t_list **stack_a, t_list **stack_b)
 	while (*stack_b)
 		ft_push(stack_a, stack_b, PA);
 }
+*/
+
+//	COUNT NUMBER SORTED
+//unsigned int	ft_count_is_sorted(t_list *stack_a)
+//{
+//	t_list	*i;
+//	unsigned int	count;
+//
+//	count = 0;
+//	i = stack_a;
+//	while (i->next && i->nbr < i->next->nbr)
+//	{
+//		i = i->next;
+//		count++;
+//	}
+//	return (count);
+//}
+//
