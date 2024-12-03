5bbe55817af0b08b988bd5076019a362c0a8d09e
Rui Alexandre Andrade Goncalves <randrade@c1r6s4.42lisboa.com>
2024-12-03 16:55:20 +0000

    3/12 16:55

--- bonus/checker.c
+++ bonus/checker.c
@@ -6,13 +6,13 @@
 /*   By: randrade <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/30 00:34:36 by randrade          #+#    #+#             */
-/*   Updated: 2024/12/03 12:13:07 by randrade         ###   ########.fr       */
+/*   Updated: 2024/12/03 16:53:07 by randrade         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../src/push_swap.h"
 
-static void	ft_move(t_list **stack_a, t_list **stack_b, char *move)
+static bool	ft_move(t_list **stack_a, t_list **stack_b, char *move)
 {
 	if (ft_strncmp(move, "sa\n", 3) == 0)
 		ft_swap(stack_a, stack_b, NULL, SA);
@@ -36,6 +36,21 @@ static void	ft_move(t_list **stack_a, t_list **stack_b, char *move)
 		ft_reverse_rotate(stack_a, stack_b, NULL, RRB);
 	else if (ft_strncmp(move, "rrr\n", 4) == 0)
 		ft_reverse_rotate(stack_a, stack_b, NULL, RRR);
+	else
+		return (false);
+	return (true);
+}
+
+static void	ft_check_move(t_list **stack_a, t_list **stack_b, char *move)
+{
+	if (move)
+	{
+		if (ft_move(stack_a, stack_b, move) == false)
+		{
+			ft_lstclear(stack_b);
+			ft_error_free_exit(NULL, stack_a);
+		}
+	}
 }
 
 static void	ft_make_moves(t_list **stack_a, t_list **stack_b)
@@ -43,14 +58,12 @@ static void	ft_make_moves(t_list **stack_a, t_list **stack_b)
 	char	*move;
 
 	move = get_next_line(0);
-	if (move)
-		ft_move(stack_a, stack_b, move);
+	ft_check_move(stack_a, stack_b, move);
 	while (move)
 	{
 		free(move);
 		move = get_next_line(0);
-		if (move)
-			ft_move(stack_a, stack_b, move);
+		ft_check_move(stack_a, stack_b, move);
 	}
 }
 
@@ -68,7 +81,7 @@ int	main(int argc, char *argv[])
 	if (!stack_a)
 		return (1);
 	ft_make_moves(&stack_a, &stack_b);
-	if (ft_is_sorted(stack_a) == true)
+	if (ft_is_sorted(stack_a) == true && stack_b == NULL)
 		ft_printf("OK\n");
 	else
 		ft_printf("KO\n");
